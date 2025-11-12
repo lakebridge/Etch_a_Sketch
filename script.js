496bfaf633c556013f3bb76f1211d4a8de7853ad
leirsam <leirsam@gmail.com>
2025-11-12 21:07:01 +0100

    created container and divs styles and created in js a loop for the 16 divs

---
+++ script.js
@@ -0,0 +1,8 @@
+div.classList.add("container");
+
+for (let i=0; i<16; i++; i=1) {
+    const divs = document.createElement("div")
+    divs.classList.add("divs");
+
+    document.div.appendChild("divs");
+}
