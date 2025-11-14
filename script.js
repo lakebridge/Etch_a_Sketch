f2fca06ce43109a89d2f20f1ebf5d804dc4e2270
leirsam <leirsam@gmail.com>
2025-11-14 01:30:35 +0100

    Set 16*16 squares instead of 4*4 - modified script as well on the loop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const div = document.querySelector("#container1");
 div.classList.add("container");
 
-for (let i=0; i<16; i++) {
+for (let i=0; i<16*16; i++) {
     const squares = document.createElement("div")
     squares.classList.add("squares");
 
