3e7bb765d77d3b2970c2b1e0a2f4ed43ee1478a8
leirsam <leirsam@gmail.com>
2025-11-18 19:08:08 +0100

    fixed the problem of opacity through attaching dataset to each square that stores the opacity fifigure and also fixed the intial size of the grid to 16 with functionning sketching option by puting the call of the function after the function and but addin if (e) before uuploading the imput and also before prevent default

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ function drawSquares(numOfSquares) {
     for (let i=0; i<numOfSquares*numOfSquares; i++) {
     const squares = document.createElement("div")
     squares.classList.add("squares");
-
+    let width = (1/numOfSquares)*100;
+    console.log(width)
+    squares.style.width = width + "%";
     div.appendChild(squares);
-
 
 }
 }
@@ -22,29 +23,54 @@ function removeSquares(style) {
 
 }
 
-drawSquares(numOfSquares);
-
-const squares = document.querySelectorAll(".squares")
+// drawSquares(numOfSquares);
 
 
-squares.forEach(square => {
-square.addEventListener('mouseover', () => {
-square.classList.add("changeColor");
-})
+// const squares = document.querySelectorAll(".squares")
 
-});
+
+// squares.forEach(square => {
+// square.addEventListener('mouseover', () => {
+// square.classList.add("changeColor");
+// })
+
+// });
 
 
 const inp = document.querySelector("input");
 const btn = document.querySelector("button");
 
+
 function respToBtn(e) {
-    e.preventDefault();
-    numOfSquares = inp.value;
-    inp.value = 0;
+    if (e) {
+        e.preventDefault();
+        numOfSquares = inp.value;
+        inp.value = 0;}
+    
     console.log(numOfSquares);
     removeSquares(".squares");
+    drawSquares(numOfSquares);
+    const squares = document.querySelectorAll(".squares")
+
+    
+
+    squares.forEach(square => {
+    
+    square.dataset.opacity = 0;
+    
+    square.addEventListener('mouseover', () => {
+    let current = Number(square.dataset.opacity);
+    current = current >= 1 ? 1 : current + 0.1
+    square.classList.add("changeColor");
+    
+    square.dataset.opacity = current;
+    square.style.opacity = current;
+                                            })
+
+        });
 
 }
 
+
+respToBtn();
 btn.addEventListener("click", respToBtn)
