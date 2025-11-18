23ca9701b48bb44abd3e41a0e4225626338a9821
leirsam <leirsam@gmail.com>
2025-11-18 17:00:03 +0100

    added removing the exisiting  squares after entering an input

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 const div = document.querySelector("#container1");
 div.classList.add("container");
 
+let numOfSquares = 16
 
-for (let i=0; i<16*16; i++) {
+function drawSquares(numOfSquares) { 
+
+    for (let i=0; i<numOfSquares*numOfSquares; i++) {
     const squares = document.createElement("div")
     squares.classList.add("squares");
 
     div.appendChild(squares);
+
+
 }
+}
+
+function removeSquares(style) {
+    
+    const squares = document.querySelectorAll(style);
+    squares.forEach(square => square.remove());
+
+}
+
+drawSquares(numOfSquares);
 
 const squares = document.querySelectorAll(".squares")
 
+
 squares.forEach(square => {
-    square.addEventListener('mouseover', () => {
-    square.classList.add("changeColor");
+square.addEventListener('mouseover', () => {
+square.classList.add("changeColor");
 })
 
 });
 
+
 const inp = document.querySelector("input");
+const btn = document.querySelector("button");
 
 function respToBtn(e) {
     e.preventDefault();
     numOfSquares = inp.value;
     inp.value = 0;
+    console.log(numOfSquares);
+    removeSquares(".squares");
 
 }
+
+btn.addEventListener("click", respToBtn)
